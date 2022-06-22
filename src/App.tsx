cd4468b2569b1c7885721175e9c39fb3947b21cf
Eduardo Faria <z.eduardofaria@gmail.com>
2022-06-22 15:35:08 -0300

    Fixing module exports in eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
-function App() {}
+import { Header } from "./components/Header";
+import { Lesson } from "./components/Lesson";
+import { Sidebar } from "./components/Sidebar";
+import { Video } from "./components/Video";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Lesson />
+      <Sidebar />
+      <Video />
+    </>
+  );
+}
 
 export default App;
