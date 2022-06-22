2eeb38c035661e3a49aaca606e130bd3f7fd697a
Eduardo Faria <z.eduardofaria@gmail.com>
2022-06-22 14:42:23 -0300

    Adding prettier, eslint and editor config

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,3 @@
-import { useQuery, gql } from '@apollo/client';
-import { useState } from 'react'
+function App() {}
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
-
-function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
-  return (
-    <ul>
-      {data?.lessons.map((lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
-  )
-}
-
-export default App
+export default App;
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,5 +2,5 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
   uri: "https://api-sa-east-1.graphcms.com/v2/cl4pl4czs1yes01z472xyc24a/master",
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
