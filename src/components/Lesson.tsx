e15d9e825b034965c4edfdc6ea27432e06d2820e
Eduardo Faria <z.eduardofaria@gmail.com>
2022-06-23 10:18:38 -0300

    Sidebar component created

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,3 +1,15 @@
 export function Lesson() {
-  return <h1>Lesson</h1>;
+  return (
+    <a href="">
+      <span>Terça • 22 de junho • 19h00</span>
+      <div>
+        <header>
+          <span>Conteúdo liberado</span>
+          <span>AO VIVO</span>
+        </header>
+
+        <strong>Abertura do evento</strong>
+      </div>
+    </a>
+  );
 }
